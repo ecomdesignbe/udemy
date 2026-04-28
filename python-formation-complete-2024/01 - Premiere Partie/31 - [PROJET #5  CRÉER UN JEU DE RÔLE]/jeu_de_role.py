aa3e4ff7d54d860d45a542225b0f84ad55313492
Steve Vandenbossche <steve@ecomdesign.be>
2026-04-28 16:44:23 +0200

    add 31 - [PROJET #5  CRÉER UN JEU DE RÔLE]

---
+++ python-formation-complete-2024/01 - Premiere Partie/31 - [PROJET #5  CRÉER UN JEU DE RÔLE]/jeu_de_role.py
@@ -0,0 +1,36 @@
+'''
+Jeu de rôle
+
+    On continue avec les jeux et cette fois-ci un projet un peu plus avanc que le simple jeu du nombre mystre.
+
+    Dans ce projet, on va créer un jeu de rôle dans le terminal.
+
+    Tu vas te battre contre un ennemi, pouvoir attaquer, prendre une potion et te faire attaquer en retour.
+
+    Beaucoup de choses gèrer donc !
+
+    Les notions utilises sont les mêmes que toutes celles que l'on a vues jusqu'à prsent :
+
+    Définir des variables
+
+    Réaliser des oprations mathématiques
+
+    Récupérer la saisie de l'utilisateur
+
+    Utiliser les boucles
+
+    Utiliser les modules (notamment le module random)
+
+    Faire des vérifications (structures conditionnelles)
+
+    Afficher des informations avec les f-string
+
+    Beaucoup de notions qui là encore individuellement ne sont pas tèrs complexes, mais que tu vas devoir agencer pour créer ce jeu de rle.
+
+    Ne va pas trop vite, écrit les étapes en français avant de te lancer directement dans du code Python et 
+    n'oublie pas de tester régulièrement ton code et afficher le type de tes variables pour progresser lentement mais sûrement !
+
+    Bonne chance
+'''
+
+todo = []
